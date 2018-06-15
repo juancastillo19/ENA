ecfdf5cbbbc628774de40dd0279f126fe7c95f2f
Juanka <Juanka@LAPTOP-8SK8797K>
2018-06-15 16:41:11 -0400

    Validar que se ingrese el usuario.

---
+++ src/java/cosa/Usuario.java
@@ -0,0 +1,86 @@
+package cosa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juanka
+ */
+public class Usuario {
+
+    /**
+     * @return the name
+     */
+   
+    private String user;
+    private String pass;
+    private String name;
+    private String lastname;
+    
+    public Usuario (String user, String pass,String name, String lastname){
+        this.user=user;
+        this.pass=pass;
+        this.name=name;
+        this.lastname=lastname;
+    }
+
+    /**
+     * @return the user
+     */
+    public String getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the pass
+     */
+    public String getPass() {
+        return pass;
+    }
+
+    /**
+     * @param pass the pass to set
+     */
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+     public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the lastname
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * @param lastname the lastname to set
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+    
+}
---
+++ src/java/cosa/ValidarDatosUserServlet.java
@@ -0,0 +1,157 @@
+package cosa;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Juanka
+ */
+@WebServlet(urlPatterns = {"/Inicio"})
+public class ValidarDatosUserServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession ses = request.getSession();
+            String user = request.getParameter("txtUser");
+            String pass = request.getParameter("txtPass");
+
+            if (user.equals("") && pass.equals("")) {
+                request.getRequestDispatcher("index.jsp?d=0").forward(request, response);
+
+            } else if (user.equals("")) {
+                request.getRequestDispatcher("index.jsp?d=1").forward(request, response);
+
+            } else if (pass.equals("")) {
+                request.getRequestDispatcher("index.jsp?d=2").forward(request, response);
+
+            } else {
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/jueves10", "root", "");
+                    PreparedStatement ps = null;
+                    ResultSet rs = null;
+                    ps = conn.prepareStatement("SELECT * FROM user WHERE user.username='" + user + "'");
+                    rs = ps.executeQuery();
+                    rs.next();
+                    //Statement st = conn.createStatement();
+                    //String query="INSERT INTO USER VALUES(2,'admin2','nombre2','apellido2')";
+                    if (rs.getString("username").equals(user)) {
+                        if (rs.getString("pass").equals(pass)) {
+                            Usuario us = new Usuario(user, pass,rs.getString("name"),rs.getString("lastname"));
+                            ses.setAttribute("usuario", us);
+                            conn.close();
+                            request.getRequestDispatcher("listausuarios.jsp").forward(request, response);
+
+                        } else {
+                            
+                        }
+                    }
+                } catch (SQLException ex) {
+                    request.getRequestDispatcher("index.jsp?d=3").forward(request, response);
+                    throw new SQLException(ex);
+                }
+                /*
+                if (user.equals("admin") && pass.equals("admin")) {
+                    Usuario us = new Usuario(user, pass);
+                    ses.setAttribute("usuario", us);
+                    request.getRequestDispatcher("Formulario.jsp").forward(request, response);
+                } else {
+                    request.getRequestDispatcher("index.jsp?d=3").forward(request, response);
+                }*/
+            }
+
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ValidarDatosUserServlet2</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ValidarDatosUserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ValidarDatosUserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ValidarDatosUserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ValidarDatosUserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
